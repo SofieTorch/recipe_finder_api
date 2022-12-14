6fd19a6c7baea99e2b79ad7a375bfecd4c7f5839
Sofia Toro <sofie.torch@outlook.com>
2022-12-14 03:39:42 -0400

    add gitignore for dataset

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 from flask import Response
 from flask import Flask
-from flask import render_template
 from flask import request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    pass
 
 if __name__ == '__main__' :
     app.run(host='0.0.0.0', port='9000', debug=True)
